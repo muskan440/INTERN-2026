5b4d6b6038999722c8e8f424267a54336854a8ba
muskan99189235 <chaurasiyamuskan361@gmail.com>
2026-06-18 11:12:03 +0530

    operators

---
+++ day10/datatype.js
@@ -0,0 +1,38 @@
+//premitive datatype ----
+
+//1string
+// let name="Muskan"
+// console.log(name);
+// console.log(typeof(name));
+
+// // 2number
+
+// let num= 26
+// console.log(num);
+// console.log(typeof(num));
+
+//3 boolean
+
+// let pass="true"
+// console.log(pass);
+// console.log(typeof(pass));
+
+//4undefined
+
+// let name
+// console.log(name);
+
+//null
+
+// let name = null
+// console.log(name);
+// console.log(typeof(name));
+
+//6symbol
+
+let id =Symbol("muskan")
+let id2 =Symbol("muskan")
+ console.log(id);
+  console.log(id2);
+console.log(id==id2);
+
---
+++ day10/operator.js
@@ -0,0 +1,49 @@
+// operators +,-,*,/,%,**,++,--
+
+
+//1 +
+// a=5,b=6,c=7
+// console.log(a+b+c);
+
+// 2 -
+
+// a=5,b=6,c=7
+// console.log(a-b-c);
+
+//*
+// a=5,b=6
+// console.log(a*b);
+
+//** 
+// a=5,b=3
+// console.log(a**b);
+
+// division
+// a=36,b=6
+// console.log(a/b);
+
+//%
+// a=38,b=6
+// console.log(a%b);
+
+//increment ++
+
+// a=28
+// a++
+// console.log(a);
+
+// b=29
+// console.log(b++);
+// console.log(b);
+
+a=11
+console.log(a--);
+console.log(a--);
+
+console.log(a--);
+console.log(a);
+
+
+
+
+
