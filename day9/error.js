8ec655aa5cb2c79d24ea8357ab3ca38823e8cc92
muskan99189235 <chaurasiyamuskan361@gmail.com>
2026-06-18 09:02:41 +0530

    js code

---
+++ day9/error.js
@@ -0,0 +1,13 @@
+ let name= "RAHUL"
+        console.log(name);
+
+        const age= 15
+        console.log(age);
+
+        var city= "delhi"
+        city="Lucknow";
+        console.log(city);
+
+        let a= 10;
+        a=20;
+        console.log(a);
---
+++ day9/hw.js
@@ -0,0 +1,21 @@
+ var name= "Muskan"
+        console.log(name);
+
+        var age=19
+        console.log(age);
+
+
+        var college= "BIET"
+        console.log(college);
+
+
+        let a= 50;
+        console.log(a);
+
+        
+         a=100;
+        console.log(a);
+
+
+        const country="INDIA"
+        console.log(country);
