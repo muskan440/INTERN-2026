b4f808a7d1d3b1ebf996e56fadfb39ce81805079
muskan99189235 <chaurasiyamuskan361@gmail.com>
2026-06-23 09:07:37 +0530

    js keys and objects

---
+++ day 13/object.js
@@ -0,0 +1,79 @@
+// //create a object
+
+// let Employee={
+//     name: "Muskan",
+//     salary: 30000,
+//     position: "Manager",
+//     address: "Deoria",
+//     joiningdate: "15-07-2026"
+
+// }
+
+// //accessing the object
+
+// console.log(Employee.joiningdate);
+
+
+
+// let CHROMA={
+//     producttype: "air conditioning",
+//     warranty: "2years",
+//     price: 40000
+// }
+// console.log(CHROMA.warranty);
+
+
+//CRUD operation.........C=CREATE, R=READ, U= UPDATE, D=DELETE
+// let actress={
+//     name:"Sanchita"
+   
+// }
+//adding
+// actress.age=22
+
+// //update
+// actress.name="Sanchita Ugale"
+// actress.profile="main actress"
+// //delete 
+
+// // delete actress.age
+//  console.log(actress);
+
+ //checking properties
+
+//   console.log("Add" in actress);
+//    console.log("name" in actress);
+
+//iterating
+let student={
+    name:"Muskan",
+    branch: "CSE",
+    rollNum:129,
+    Add: "Mahuadih"
+}
+for(let key in student ){
+    console.log(key+"---->"+student[key]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ homeworksfolder/dayyy13.js
@@ -0,0 +1,78 @@
+// //1.
+
+
+
+// let Student={
+//     name: "Muskan",
+//     age: 19,
+//     Course: "B.tech",
+//     city: "Deoria"
+// }
+// console.log(Student);
+
+// //2.updating object properties
+// Student.name="Muskan Chaurasiya"
+// Student.city="Lucknow"
+// Student.age=20
+
+// console.log(Student);
+
+// //3. Add new properties
+
+// Student.email="kjhdkjfh@gmail.com"
+// Student.MobNum= 9999999990
+// console.log(Student);
+
+// //4. deleting properties
+
+// delete Student.MobNum
+// console.log(Student);
+
+// //5. employee 
+
+// let Employee={
+//     id: "manager",
+//     name: "Shalu",
+//     department: "IT",
+//     salary: 30000
+    
+
+// }
+// console.log(Employee);
+
+let Product={
+    Productname: "Titan quartz analog with date blue dial stainless steel strap watch for women",
+    price: 3609,
+    category:"wrist watch",
+    stock:"in stock"
+
+}
+console.log(Product);
+
+Product.stock="out of stock"
+console.log(Product);
+
+ let javascript=
+{
+  name: "Rahul",
+  age: 20,
+  address:"gkp" ,
+    city: "Lucknow",
+    state: "UP"
+  
+}
+console.log(javascript);
+
+
+ let Student={
+    name: "Muskan",
+    age: 19,
+    course:"btech",
+    Addres :{
+    city: "deoria",
+    State: "UTTAR PRADESH"
+
+
+   }
+}
+console.log(Student);
