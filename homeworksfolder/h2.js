c47de94837843a97f805f215993ae66dcfa88f24
muskan99189235 <chaurasiyamuskan361@gmail.com>
2026-06-24 10:26:54 +0530

    adding something

--- homeworksfolder/h2.js
+++ homeworksfolder/h2.js
@@ -1,23 +1,35 @@
-// let fruits=[ "apple", "grapes", "pineapple", "lichi", "kiwi"
 
-// ]
-// console.log(fruits);
+//fruits name
 
 
+let fruits=[ "apple", "grapes", "pineapple", "litchi", "kiwi"
 
-// console.log( "index number is--->",fruits[0]);
+]
+console.log(fruits);
 
-// console.log( "index number is--->",fruits[4]);
 
-// fruits.push("pomegranate")
-// console.log( fruits);
+//first and last element
+console.log( "index number is--->",fruits[0]);
 
-// fruits.pop()
-// console.log(fruits);
+console.log( "index number is--->",fruits[4]);
 
+
+//adding element
+fruits.push("pomegranate")
+console.log( fruits);
+
+
+//removing element
+fruits.pop()
+console.log(fruits);
+
+
+//sum of number
 let num=[2,6,98,79,26,29]
-// console.log( "sum of total num---->",num[0]+num[1]+num[2]+num[3]+num[4]+num[5]);
+console.log( "sum of total num---->",num[0]+num[1]+num[2]+num[3]+num[4]+num[5]);
 
+
+//using for loop
 for(i=0;i<=5;i++)
   
  console.log(num[i]);
