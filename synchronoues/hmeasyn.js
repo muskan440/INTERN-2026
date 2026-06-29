5595f3fc444ccd86e357d9fdb2f93905f11fe6f7
muskan99189235 <chaurasiyamuskan361@gmail.com>
2026-06-29 22:55:37 +0530

    today work

---
+++ synchronoues/hmeasyn.js
@@ -0,0 +1,48 @@
+function start(){
+    console.log("Fill the form.... ")
+
+}
+
+function PREPARATION(){
+    setTimeout(()=>{
+    console.log("Start preparation for cracking exam also practice for interview.... ")
+
+    },40000)
+}
+
+function TakesAdmitcard(){
+    setTimeout(()=>{
+   
+        console.log("Download admit card from the website....")
+
+    },50000)
+}
+
+
+function EXAMDAY(){
+    setTimeout(()=>{
+       console.log("Go to give exam and after giving exam start preparation for interview....")
+
+
+    },60000)
+}
+
+
+ function FINAL(){
+    setTimeout(()=>{
+       console.log(" After craking exam go for crack the interview.")
+
+
+    },70000)
+} 
+   
+ function CALL(){
+    start()
+    PREPARATION()
+    TakesAdmitcard()
+    FINAL()
+   
+
+}
+
+CALL()
---
+++ synchronoues/hmesyn.js
@@ -0,0 +1,40 @@
+function start(){
+    console.log("Fill the form.... ")
+
+}
+
+function PREPARATION(){
+    console.log("Start preparation for cracking exam also practice for interview.... ")
+
+}
+
+function TakesAdmitcard(){
+   
+        console.log("Download admit card from the website....")
+
+    }
+   
+
+
+function EXAMDAY(){
+       console.log("Go to give the exam after cracking exam start preparation for interview....")
+
+
+    }
+
+ function FINAL(){
+       console.log(" After craking exam go for crack the interview.")
+
+
+    }   
+   
+ function CALL(){
+    start()
+    PREPARATION()
+    TakesAdmitcard()
+    FINAL()
+   
+
+}
+
+CALL()
