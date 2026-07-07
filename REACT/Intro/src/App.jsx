9cca4f3e63dcca4d86ab1d66048f7c6f68b4ef1f
muskan99189235 <chaurasiyamuskan361@gmail.com>
2026-07-07 11:26:11 +0530

    react

---
+++ REACT/Intro/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Greet from './Greet'
+
+const App = () => {
+  return (
+    <div>
+      <Greet />
+     
+    </div>
+  )
+}
+
+export default App
---
+++ REACT/Intro/src/Greet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <div>
+        <h1>
+            Muskan Chaurasiya
+        </h1>
+    </div>
+  )
+}
+
+export default Greet
